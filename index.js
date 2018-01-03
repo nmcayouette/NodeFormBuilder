ecacfe9dd6d732887f2fb33b4462f9bd4f17d6f1
Luke Billington <lukecreative@me.com>
2018-01-03 17:48:28 -0500

    Added mapping
    Included a map of the inputs and buildInput method.
    Renamed build method to buildForm.
    Added temporary dummy input for testing.

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-let build = (inputs, action = false, method = 'GET') => {
+// Main function to build the complete form
+let buildForm = (inputs) => {
+
+  // Iterate through each input
+  inputs.map(buildInput);
+}
+
+// Factory for building different input types
+let buildInput = (input) => {
+  console.log('Hello World! I am an input:', input.type);
 
 }
+
+let testFields = [
+  {
+    name: 'firstName',
+    type: 'text',
+    label: 'First Name'
+  },
+  {
+    name: 'lastName',
+    type: 'text',
+    label: 'Last Name'
+  },
+  {
+    name: 'firstName',
+    type: 'text',
+    label: 'First Name'
+  },
+  {
+    name: 'email',
+    type: 'email',
+    label: 'Email'
+  },
+  {
+    name: 'password',
+    type: 'password',
+    label: 'Password'
+  },
+  {
+    name: 'csrf',
+    type: 'hidden',
+    value: 'wgPbQMS_fJL@RO6ZTwc-nmS1$1a7R9L('
+  },
+  {
+    type: 'reset',
+    value: 'Clear Form'
+  },
+  {
+    type: 'submit',
+    value: 'Send Form'
+  }
+]
+buildForm(testFields);
